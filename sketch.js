e29f57287d0d1175e17440e3f11e163f1c11d7de
Ivanildo Pereira <suporte@scieve.com>
2022-12-01 09:25:46 -0300

    Iniciando no Javascript - Refatoração e funções.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
+//variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 15;
+let raio = diametro / 2;
 
+//velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
-
-let raio = diametro / 2;
 
 function setup() {
     createCanvas(600, 400);
@@ -13,10 +14,21 @@ function setup() {
 
 function draw() {
     background(0);
-    circle(xBolinha, yBolinha, diametro);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+}
+
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro)
+}
+
+function movimentaBolinha() {
     xBolinha += velocidadeXBolinha;
     yBolinha += velocidadeYBolinha;
+}
 
+function verificaColisaoBorda() {
     if (xBolinha + raio > width || xBolinha - raio < 0) {
         velocidadeXBolinha *= -1;
     }
@@ -24,4 +36,3 @@ function draw() {
         velocidadeYBolinha *= -1;
     }
 }
-
